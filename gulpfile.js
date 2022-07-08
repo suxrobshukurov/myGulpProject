88336b14fcb5c1f9c857043fa607b9ca02e0f4e1
Suxrob Shukurov <sdead96@gmail.com>
2022-07-08 14:51:35 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+// Основной модуль
+import gulp from 'gulp';
+// Импорт путей
+import { path } from './gulp/config/path.js';
+// Импорт общих плагинов
+import { plugins } from './gulp/config/plugins.js';
+
+global.app = {
+  isBuild: process.argv.includes('--build'),
+  isDev: !process.argv.includes('--build'),
+  path,
+  gulp,
+  plugins,
+};
+
+// Импорт задачи
+import { copy } from './gulp/tasks/copy.js';
+import { reset } from './gulp/tasks/reset.js';
+import { html } from './gulp/tasks/html.js';
+import { server } from './gulp/tasks/server.js';
+import { scss } from './gulp/tasks/scss.js';
+import { js } from './gulp/tasks/js.js';
+import { images } from './gulp/tasks/images.js';
+import { otfToTtf, ttfToWoff, fontsStyle } from './gulp/tasks/fonts.js';
+import { svgSprive } from './gulp/tasks/svgSprive.js';
+
+// Наблюдатель за изменениями в файлах
+function watcher() {
+  gulp.watch(path.watch.files, copy);
+  gulp.watch(path.watch.html, html);
+  gulp.watch(path.watch.scss, scss);
+  gulp.watch(path.watch.js, js);
+  gulp.watch(path.watch.images, images);
+}
+
+// последовательная обработка шрифтов
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
+
+const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
+
+// Построение сценариев выполнения задач
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
+const build = gulp.series(reset, mainTasks);
+
+// Экспорт сценариев
+export { svgSprive };
+export { dev };
+export { build };
+
+// Выполнение сценария по умолчанию
+gulp.task('default', dev);
---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+import * as flsFunction from './modules/functions.js';
+
+flsFunction.isWebp();
+
+import Swiper from 'swiper';
+// import Swiper and modules styles
+
+new Swiper('.mySwiper', {
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+console.log('work');
